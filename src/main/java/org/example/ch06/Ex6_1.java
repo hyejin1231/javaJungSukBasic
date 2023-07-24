c3a9f4c60e66c46b88e9e7f217d187f40fde90d9
hyejin1231 <phj981231@naver.com>
2023-07-24 09:02:51 +0900

    2023.07.24 ch6. 클래스와 객체, 한 파일에 여러 클래스 작성

---
+++ src/main/java/org/example/ch06/Ex6_1.java
@@ -0,0 +1,13 @@
+package org.example.ch06;
+
+/**
+ * ch06. 클래스와 객체
+ * ex) 클래스와 객체는 설계도와 제품으로 예를 들 수 있다.
+ */
+public class Ex6_1 // 소스파일의 이름은 public class 이름과 일치해야 한다.
+{
+}
+
+class Ex6_1_Temp {} // 하나의 소스파일에는 하나의 클래스만 작성하는 것이 바람직하다.
+
+//public class Ex6_2_temp{} // 하나의 소스파일에는 하나의 public class만 허용한다.
