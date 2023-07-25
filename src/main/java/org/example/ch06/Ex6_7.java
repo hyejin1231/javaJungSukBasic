a158705f865dc4542634a595003fbc2df25d49a9
hyejin1231 <phj981231@naver.com>
2023-07-25 10:03:12 +0900

    2023.07.25 ch6. return문, 반환값

--- src/main/java/org/example/ch06/Ex6_7.java
+++ src/main/java/org/example/ch06/Ex6_7.java
@@ -1,7 +1,9 @@
 package org.example.ch06;
 
 /**
- * ch06. 메서드 호출
+ * ch06. 메서드 호출, return문, 반환값
+ * return문 : 실행중인 메서드를 종료하고 호출했던 곳으로 돌아간다.
+ * 반환값 : 타입이 일치해야 한다!!
  */
 public class Ex6_7
 {
@@ -25,18 +27,23 @@ public class Ex6_7
 		System.out.println("max = " + max);
 		System.out.println("min = " + min);
 		
+		myMath.printGuGudan(12);
+		myMath.printGuGudan(9);
+		
+		
 	}
 }
 
 class MyMath {
+	
 	long add(long a, long b)
 	{
-		return a + b;
+		return a + b; // 반환타입이 void가 아닌 경우에는 return문 생략 불가능
 	}
 	
 	long subtract(long a, long b)
 	{
-		return a - b;
+		return a - b; // 반환값의 타입이 일치해야한다!!
 	}
 	
 	long multiply(long a, long b)
@@ -57,5 +64,18 @@ class MyMath {
 	long min(long a, long b)
 	{
 		return (a < b) ? a : b;
+	}
+	
+	void printGuGudan(int dan)
+	{
+		if (!(dan >= 2 && dan <= 9)) {
+			return;
+		}
+		
+		for (int i = 1; i<= 9; i++)
+		{
+			System.out.printf("%d * %d = %d%n", dan, i, (dan * i));
+		}
+		// return; // 생략 가능
 	}
 }
