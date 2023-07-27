03c3cc8c0a366ede55d264ce8172f328e8b8fd12
hyejin1231 <phj981231@naver.com>
2023-07-28 08:56:52 +0900

    2023.07.28 ch7. 패키지, 클래스 패스

---
+++ src/main/java/org/example/ch07/PackageTest.java
@@ -0,0 +1,14 @@
+package org.example.ch07;
+
+/**
+ * ch07. 패키지, 클래스 패스
+ * 패키지 : 서로 관련 있는 클래스들의 묶음
+ * 클래스 패스 : 클래스 파일 (*.class)의 위치를 알려주는 경로  -> 환경 변수 classpath에 등록해줘야 함
+ */
+public class PackageTest
+{
+	public static void main(String[] args)
+	{
+		System.out.println("PackageTest.main");
+	}
+}
