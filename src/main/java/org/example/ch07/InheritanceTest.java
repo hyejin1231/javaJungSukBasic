274febf14b4025d209fd97e8fa2119b6dcabdbc7
hyejin1231 <phj981231@naver.com>
2023-07-27 09:26:50 +0900

    2023.07.27 ch7. 클래스 간의 관계, 상속과 포함

---
+++ src/main/java/org/example/ch07/InheritanceTest.java
@@ -0,0 +1,46 @@
+package org.example.ch07;
+
+/**
+ * ch07. 클래스간의 관계, 상속과 포함
+ * 포함 관계
+ * : 클래스의 멤버로 참조변수를 선언하는 것
+ */
+class MyPoint {
+	int x;
+	int y;
+}
+
+class MyCircle extends MyPoint { // 상속 관계
+	int r;
+}
+
+class MyCircle2 { // 포함 관계
+	MyPoint point = new MyPoint(); // 참조변수의 초기화
+	int r;
+}
+
+public class InheritanceTest
+{
+	public static void main(String[] args)
+	{
+		// 상속 관계
+		MyCircle circle = new MyCircle();
+		circle.x =1;
+		circle.y = 2;
+		circle.r = 3;
+		
+		System.out.println("circle.x = " + circle.x);
+		System.out.println("circle.y = " + circle.y);
+		System.out.println("circle.r = " + circle.r);
+		
+		// 포함 관계
+		MyCircle2 myCircle2 = new MyCircle2();
+		myCircle2.point.x = 1;
+		myCircle2.point.y = 2;
+		myCircle2.r =3;
+		
+		System.out.println("myCircle2.point.x = " + myCircle2.point.x);
+		System.out.println("myCircle2.point.y = " + myCircle2.point.y);
+		System.out.println("myCircle2.r = " + myCircle2.r);
+	}
+}
