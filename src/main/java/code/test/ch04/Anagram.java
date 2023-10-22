ee943c2a72d7bd6684ab01eac670ae0027ad6fb0
hyejin <phj981231@naver.com>
2023-10-22 20:41:37 +0900

    2023.10.22. 알고리즘 연습 ch04. HashMap, TreeSet (해쉬, 정렬지원 Set) : 아나그램

---
+++ src/main/java/code/test/ch04/Anagram.java
@@ -0,0 +1,91 @@
+package code.test.ch04;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * ch04. HashMap, TreeSet (해쉬, 정렬지원 Set)
+ * : 아나그램
+ *
+ * 설명
+ * Anagram이란 두 문자열이 알파벳의 나열 순서를 다르지만 그 구성이 일치하면 두 단어는 아나그램이라고 합니다.
+ * 예를 들면 AbaAeCe 와 baeeACA 는 알파벳을 나열 순서는 다르지만 그 구성을 살펴보면 A(2), a(1), b(1), C(1), e(2)로
+ * 알파벳과 그 개수가 모두 일치합니다. 즉 어느 한 단어를 재 배열하면 상대편 단어가 될 수 있는 것을 아나그램이라 합니다.
+ * 길이가 같은 두 개의 단어가 주어지면 두 단어가 아나그램인지 판별하는 프로그램을 작성하세요. 아나그램 판별시 대소문자가 구분됩니다.
+ *
+ * 입력
+ * 첫 줄에 첫 번째 단어가 입력되고, 두 번째 줄에 두 번째 단어가 입력됩니다.
+ * 단어의 길이는 100을 넘지 않습니다.
+ *
+ * 출력
+ * 두 단어가 아나그램이면 “YES"를 출력하고, 아니면 ”NO"를 출력합니다.
+ *
+ * 예시 입력 1
+ * AbaAeCe
+ * baeeACA
+ *
+ * 예시 출력 1
+ * YES
+ *
+ * 예시 입력 2
+ * abaCC
+ * Caaab
+ *
+ * 예시 출력 2
+ * NO
+ */
+public class Anagram {
+    public static void main(String[] args) {
+        Anagram anagram = new Anagram();
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+        String b = scanner.nextLine();
+
+        String result = anagram.solution(a, b);
+        System.out.print(result);
+
+        System.out.println();
+        String result2 = anagram.solution2(a, b);
+        System.out.println("result2 = " + result2);
+    }
+
+    public String solution(String a, String b) {
+        String answer = "YES";
+        Map<Character, Integer> mapA = new HashMap<>();
+        Map<Character, Integer> mapB = new HashMap<>();
+        for (char c : a.toCharArray()) {
+            mapA.put(c, mapA.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : b.toCharArray()) {
+            mapB.put(c, mapB.getOrDefault(c, 0) + 1);
+        }
+
+        for (Character x : mapA.keySet()) {
+            if (mapA.get(x) != mapB.get(x)) {
+                return "NO";
+            }
+        }
+
+        return answer;
+    }
+
+    public String solution2(String s1, String s2) {
+        String answer = "YES";
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char x : s1.toCharArray()) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+
+        for (char x : s2.toCharArray()) {
+            if (!map.containsKey(x) || map.get(x) == 0) {
+                return "NO";
+            }
+            map.put(x, map.get(x) - 1);
+        }
+
+
+        return answer;
+    }
+}
