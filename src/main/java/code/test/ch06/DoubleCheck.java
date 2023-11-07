34c9a5f71acb35fe7f2a98cdd61f923b047ec336
hyejin1231 <phj981231@naver.com>
2023-11-07 13:51:47 +0900

    2023.11.07. 알고리즘 연습 ch06. Sorting and Searching(정렬, 이분검색과 결정알고리즘) : 중복 확인

---
+++ src/main/java/code/test/ch06/DoubleCheck.java
@@ -0,0 +1,87 @@
+package code.test.ch06;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * ch06. Sorting and Searching(정렬, 이분검색과 결정알고리즘)
+ * : 중복 확인
+ *
+ * 설명
+ * 현수네 반에는 N명의 학생들이 있습니다.
+ * 선생님은 반 학생들에게 1부터 10,000,000까지의 자연수 중에서 각자가 좋아하는 숫자 하나 적어 내라고 했습니다.
+ * 만약 N명의 학생들이 적어낸 숫자 중 중복된 숫자가 존재하면 D(duplication)를 출력하고,
+ * N명이 모두 각자 다른 숫자를 적어냈다면 U(unique)를 출력하는 프로그램을 작성하세요.
+ *
+ * 입력
+ * 첫 번째 줄에 자연수 N(5<=N<=100,000)이 주어진다.
+ * 두 번째 줄에 학생들이 적어 낸 N개의 자연수가 입력된다.
+ *
+ * 출력
+ * 첫 번째 줄에 D 또는 U를 출력한다.
+ *
+ * 예시 입력 1
+ * 8
+ * 20 25 52 30 39 33 43 33
+ *
+ * 예시 출력 1
+ * D
+ */
+public class DoubleCheck
+{
+	public static void main(String[] args)
+	{
+		DoubleCheck doubleCheck = new DoubleCheck();
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int[] arr = new int[n];
+		
+		for (int i = 0; i < n; i++)
+		{
+			arr[i] = scanner.nextInt();
+		}
+		
+		String result = doubleCheck.solution(n, arr);
+		System.out.print(result);
+		
+		System.out.println();
+		String result2 = doubleCheck.solution2(n, arr);
+		System.out.println("result2 = " + result2);
+		
+	}
+	
+	public String solution(int n, int[] arr)
+	{
+		String answer = "U";
+		ArrayList<Integer> arrayList = new ArrayList<>();
+		
+		for (int i : arr)
+		{
+			if (!arrayList.contains(i)) {
+				arrayList.add(i);
+			}else
+			{
+				return "D";
+			}
+		}
+		
+		return answer;
+	}
+	
+	// 정렬로 푸는 방법 nlogn
+	public String solution2(int n, int[] arr)
+	{
+		String answer = "U";
+		Arrays.sort(arr);
+		
+		for (int i = 0; i< n-1; i++)
+		{
+			if (arr[i] == arr[i + 1]) {
+				return "D";
+			}
+		}
+		
+		return answer;
+	}
+}
