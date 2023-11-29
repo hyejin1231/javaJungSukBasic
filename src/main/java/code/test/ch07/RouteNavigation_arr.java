e5f9d6360e7f5512498e26ded25bab38198fb4ee
hyejin1231 <phj981231@naver.com>
2023-11-29 15:53:24 +0900

    2023.11.28. 알고리즘 연습 ch07.섹션 7. Recursive, Tree, Graph(DFS, BFS 기초) : 경로 탐색 (인접리스트)

--- src/main/java/code/test/ch07/RouteNavigation_arr.java
+++ src/main/java/code/test/ch07/RouteNavigation_arr.java
@@ -6,8 +6,14 @@ import java.util.Scanner;
  * ch07.섹션 7. Recursive, Tree, Graph(DFS, BFS 기초) : 경로 탐색 (인접행렬)
  *
  * 설명 방향그래프가 주어지면 1번 정점에서 N번 정점으로 가는 모든 경로의 가지 수를 출력하는 프로그램을 작성하세요. 아래 그래프에서 1번
- * 정점에서 5번 정점으로 가는 가지 수는 1 2 3 4 5 1 2 5 1 3 4 2 5 1 3 4 5 1 4 2 5 1 4 5 총 6
- * 가지입니다.
+ * 정점에서 5번 정점으로 가는 가지 수는
+ * 1 2 3 4 5
+ * 1 2 5
+ * 1 3 4 2 5
+ * 1 3 4 5
+ * 1 4 2 5
+ * 1 4 5
+ * 총 6가지입니다.
  *
  * 각 경로의 길이는 루트노드에서 말단노드까지 가는데 이동하는 횟수를 즉 간선(에지)의 개수를 길이로 하겠습니다.
  *
@@ -15,11 +21,21 @@ import java.util.Scanner;
  *
  * 출력 설명 총 가지수를 출력한다.
  *
- * 입력 예제 1 5 9 1 2 1 3 1 4 2 1 2 3 2 5 3 4 4 2 4 5
+ * 입력 예제
+ * 5 9
+ * 1 2
+ * 1 3
+ * 1 4
+ * 2 1
+ * 2 3
+ * 2 5
+ * 3 4
+ * 4 2
+ * 4 5
  *
  * 출력 예제 1 6
  */
-public class RouteNavigation
+public class RouteNavigation_arr
 {
 	static int n, m, cnt = 0;
 	static int[][] graph;
@@ -45,7 +61,7 @@ public class RouteNavigation
 	
 	public static void main(String[] args)
 	{
-		RouteNavigation routeNavigation = new RouteNavigation();
+		RouteNavigation_arr routeNavigation = new RouteNavigation_arr();
 		Scanner scanner = new Scanner(System.in);
 		n = scanner.nextInt();
 		m = scanner.nextInt();
---
+++ src/main/java/code/test/ch07/RouteNavigation_list.java
@@ -0,0 +1,16 @@
+package code.test.ch07;
+
+import java.util.Scanner;
+
+/**
+ * ch07.섹션 7. Recursive, Tree, Graph(DFS, BFS 기초) : 경로 탐색 (인접리스트)
+ */
+public class RouteNavigation_list
+{
+	public static void main(String[] args)
+	{
+		RouteNavigation_list routeNavigationList = new RouteNavigation_list();
+		Scanner scanner = new Scanner(System.in);
+	
+	}
+}
